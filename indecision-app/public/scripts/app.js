75c6d0c9c7024515c33dc6adbb758ceb32df7c0d
Matthew Brown <matthew@freezeproshop.com>
2017-11-06 19:25:25 +0000

    first challenge

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,10 +1,51 @@
 "use strict";
 
+// JSX
+
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Indecision App"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is Some Info"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "one"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "two"
+        )
+    )
 );
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Matthew Brown"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: Bristol"
+    )
+);
+
 var approot = document.getElementById("app");
 
-ReactDOM.render(template, approot);
+ReactDOM.render(templateTwo, approot);
--- indecision-app/src/app.jsx
+++ indecision-app/src/app.jsx
@@ -1,4 +1,23 @@
-var template = <h1>Indecision App</h1>;
+// JSX
+
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is Some Info</p>
+        <ol>
+            <li>one</li>
+            <li>two</li>
+        </ol>
+    </div>
+);
+
+let templateTwo = (
+    <div>
+        <h1>Matthew Brown</h1>
+        <p>Location: Bristol</p>
+    </div>
+);
+
 var approot = document.getElementById("app");
 
-ReactDOM.render(template, approot);
+ReactDOM.render(templateTwo, approot);
